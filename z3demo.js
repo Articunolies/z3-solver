430ebfd2d51167dd87781cbb0c4e2e1f4cb88e48
Derek Simpson <articunolies22@gmail.com>
2024-11-06 15:12:27 -0800

    Represented all 3 Game Constraints

--- z3demo.js
+++ z3demo.js
@@ -2,17 +2,42 @@ import { init } from 'z3-solver';
 
 const { Context } = await init();
 const { Solver, Int, And, Or, Distinct } = new Context("main");
-const solver = new Solver();
 
-const x = Int.const('x');  // x is a Z3 integer
+// Inside the Fence
+let solver = new Solver();
+let x = Int.const('x');
+let y = Int.const('y');
 
-solver.add(And(x.le(10), x.ge(9)));  // x <= 10, x >=9
+solver.add(And(x.gt(5), x.lt(10), y.gt(15), y.lt(25)));
 
-// Run Z3 solver, find solution and sat/unsat
 console.log(await solver.check());
+let model = solver.model();
+let xVal = model.eval(x);
+let yVal = model.eval(y);
+console.log(`Inside the fence: x = ${xVal}, y = ${yVal}`);
 
-// Extract value for x
-const model = solver.model();
-const xVal = model.eval(x);
-console.log(`${xVal}`);
+// On the Fence
+solver = new Solver();
+x = Int.const('x');
+y = Int.const('y');
 
+solver.add(Or(And(x.eq(5), y.gt(15), y.lt(25)), And(y.eq(15), x.gt(5), x.lt(10))));
+
+console.log(await solver.check());
+model = solver.model();
+xVal = model.eval(x);
+yVal = model.eval(y);
+console.log(`On the fence: x = ${xVal}, y = ${yVal}`);
+
+// Outside the Fence
+solver = new Solver();
+x = Int.const('x');
+y = Int.const('y');
+
+solver.add(And(Or(x.lt(5), x.gt(10), y.lt(15), y.gt(25)), x.ge(8), y.ge(20)));
+
+console.log(await solver.check());
+model = solver.model();
+xVal = model.eval(x);
+yVal = model.eval(y);
+console.log(`Outside the fence: x = ${xVal}, y = ${yVal}`);
